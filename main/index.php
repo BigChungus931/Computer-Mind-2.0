759aa1a7e2c9f7c3cb0355069561053e10da7194
BigChungus931 <borsoshkin@gmail.com>
2025-10-18 13:08:15 -0600

    Session.php

--- main/index.php
+++ main/index.php
@@ -1,16 +1,5 @@
 <?php
-session_start();
-if (isset($_GET['action']) && $_GET['action'] == "logout") {
-    session_unset();
-    session_destroy();
-    header("Location: ../login.php");
-    exit();
-} 
-
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header("Location: ../login.php");
-    exit();
-}
+include "include/session.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
