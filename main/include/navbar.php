86d336022849868ea1790c8cfc6b46401172cfb9
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2025-10-20 14:07:37 +0200

    Navbar

--- main/include/navbar.php
+++ main/include/navbar.php
@@ -4,21 +4,24 @@
         class="logo"
         src="imgs/Computer-logo.PNG"
         alt="Computer mind logo" />  
-    <button class="hamburger" aria-controls="sidebar" aria-expanded="false" onclick="hamburgerf()">≣</button>
-
+    
     <div class="nav-right">
-        <i class="fa-solid fa-user-circle user-icon" aria-hidden="true"></i>
-        <span class="welcome-text">Welcome <?php echo $_SESSION['firstname']; ?></span>
-        <button class="theme-toggle" type="button" aria-label="Switch theme" onclick="changeTheme()">
-            <i class="fa-solid fa-moon"></i>
-        </button>
+        
         <div class="dropdown">
-            <button class="account-btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Account
+            <button class="icon-btn user-toggle dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" aria-label="Account menu">
+                <i class="fa-solid fa-user-circle user-icon" aria-hidden="true"></i>
+                <span class="user-label"><span class="hide-sm">Welcome <?php echo $_SESSION['firstname']; ?></span></span>
             </button>
             <ul class="dropdown-menu dropdown-menu-end">
                 <li><a href="?action=logout" class="dropdown-item">Logout</a></li>
             </ul>
         </div>
+
+        <button class="theme-toggle" type="button" aria-label="Switch theme" onclick="changeTheme()">
+            <i class="fa-solid fa-moon"></i>
+        </button>
+
+        <button class="hamburger" aria-controls="sidebar" aria-expanded="false" onclick="hamburgerf()">≣</button>
+
     </div>
 </nav>
