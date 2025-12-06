e3e10102898f611c84f99e44a18ad57dcdd98728
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2025-12-06 20:24:37 +0200

    Custom CSS for the Form

--- main/Panel/edit.php
+++ main/Panel/edit.php
@@ -12,6 +12,7 @@ include "../include/session.php";
   <link rel="stylesheet" href="<?php echo BASE_URL ?>Bootstrap/bootstrap.css" />
   <script src="<?php echo BASE_URL ?>js/load.js"></script>
   <link rel="stylesheet" href="<?php echo BASE_URL ?>css/style.css" />
+  <link rel="stylesheet" href="<?php echo BASE_URL ?>css/form.css" />
   <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
@@ -36,54 +37,40 @@ include "../include/session.php";
     <section class="about-section mb-5">
       <h1 class="section-title">Edit <?php echo $user["Firstname"] . " " . $user["Lastname"] ?></h1>
     </section>
-    <div class="container">
-      <form>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputEmail4">Email</label>
-            <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-          </div>
-          <div class="form-group col-md-6">
-            <label for="inputPassword4">Password</label>
-            <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="inputAddress">Address</label>
-          <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-        </div>
-        <div class="form-group">
-          <label for="inputAddress2">Address 2</label>
-          <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-        </div>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputCity">City</label>
-            <input type="text" class="form-control" id="inputCity">
-          </div>
-          <div class="form-group col-md-4">
-            <label for="inputState">State</label>
-            <select id="inputState" class="form-control">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>
-          </div>
-          <div class="form-group col-md-2">
-            <label for="inputZip">Zip</label>
-            <input type="text" class="form-control" id="inputZip">
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck">
-            <label class="form-check-label" for="gridCheck">
-              Check me out
-            </label>
-          </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
-      </form>
+   <div class="container">
+  <form method="POST" action="update.php">
+    <input type="hidden" name="id" value="<?php echo $user['ID']; ?>">
+    
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <label for="firstname" class="form-label">First Name</label>
+        <input type="text" class="form-control" id="firstname" name="fname" value="<?php echo htmlspecialchars($user['Firstname']); ?>">
+      </div>
+      <div class="col-md-6 mb-3">
+        <label for="lastname" class="form-label">Last Name</label>
+        <input type="text" class="form-control" id="lastname" name="lname" value="<?php echo htmlspecialchars($user['Lastname']); ?>">
+      </div>
     </div>
+    
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($user['Email']); ?>">
+      </div>
+      <div class="col-md-6 mb-3">
+        <label for="age" class="form-label">Age</label>
+        <input type="number" class="form-control" id="age" name="age" value="<?php echo $user['Age']; ?>">
+      </div>
+    </div>
+    
+    <div class="mb-3">
+      <label for="country" class="form-label">Country</label>
+      <input type="text" class="form-control" id="country" name="country" value="<?php echo htmlspecialchars($user['Country']); ?>">
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Update User</button>
+  </form>
+</div>
   </div>
 </body>
 
