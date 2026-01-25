b7e0afbaa464a3cdf1e4302d39423a7feb72611a
BigChungus931 <borsoshkin@gmail.com>
2026-01-25 11:02:00 -0600

    Made it so the admins can send messages to users and others.

--- main/Panel/view_notification.php
+++ main/Panel/view_notification.php
@@ -25,29 +25,37 @@ include "../include/session.php";
   <div class="main-content">
     <section class="notify-section">
       <h1 class="section-title">Notification</h1>
-      <div class="notifycard">
+      <?php
+      $notify = $conn->prepare("SELECT * FROM notifications WHERE user_id = ?");
+      $notify->bind_param("i", $id);
+      $notify->execute();
+      $notify_result = $notify->get_result();
+      while ($notification = $notify_result->fetch_assoc()) {
+        echo '
+          <div class="notifycard">
         <div class="header">
-          <?php
-          if ($auth_user["Gender"] == "male") {
-            echo '<img src="https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png" alt="">';
-          } else if ($auth_user["Gender"] == "female") {
-            echo '<img src="https://cdn1.iconfinder.com/data/icons/website-internet/48/website_-_female_user-1024.png" alt="">';
-          } else {
-            echo '<img src="https://tse1.mm.bing.net/th/id/OIP.xfzEU2lyX83UXOn6-bY0KQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" alt="">';
-          }
-          ?>
+          ';
+        if ($auth_user["Gender"] == "male") {
+          echo '<img src="https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png" alt="">';
+        } else if ($auth_user["Gender"] == "female") {
+          echo '<img src="https://cdn1.iconfinder.com/data/icons/website-internet/48/website_-_female_user-1024.png" alt="">';
+        } else {
+          echo '<img src="https://tse1.mm.bing.net/th/id/OIP.xfzEU2lyX83UXOn6-bY0KQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" alt="">';
+        }
+
+        echo '
           <h5>
-            Dexter Schiller
+            User
           </h5>
         </div>
         <div class="sub-header">
           <p>
-            This is a notification detail page.
+            ' . $notification["message"] . '
           </p>
-          <span>10 minutes ago</span>
+          <span>' . time() - strtotime($notification["created_at"]) . ' seconds ago</span>
         </div>
-      </div>
-      <?php
+      </div> ';
+      }
       ?>
     </section>
   </div>
--- main/include/notify.php
+++ main/include/notify.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_POST["id"];
     $msg = $_POST["message"];
     $seen = "no";
-    $date = date("Y-m-d");
+    $date = date("Y-m-d H:i:s");
     $add_notify = $conn->prepare("INSERT INTO notifications (User_ID, Message, Seen, created_at) VALUES (?, ?, ?, ?)");
     $add_notify->bind_param("isss", $id, $msg, $seen, $date);
     $add_notify->execute();
