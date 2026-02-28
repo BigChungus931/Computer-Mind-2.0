67a2d98a6a282c80d11a9feeed7a3aa0ab6df3cc
BigChungus931 <borsoshkin@gmail.com>
2026-02-28 12:49:05 -0600

    Added more content

--- main/about.php
+++ main/about.php
@@ -7,15 +7,22 @@ include "include/session.php";
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>About Us</title>
-    <link href="<?php echo BASE_URL ?>img/favicon.png" rel="icon">
-    <link rel="stylesheet" href="<?php echo BASE_URL ?>Bootstrap/bootstrap.css" />
-    <script src="<?php echo BASE_URL ?>js/load.js"></script>
-    <link rel="stylesheet" href="<?php echo BASE_URL ?>css/style.css" />
-    <link rel="stylesheet" href="<?php echo BASE_URL ?>css/form.css" />
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    <title>Computer Mind</title>
+    <link href="img/favicon.png" rel="icon">
+    <script src="js/load.js"></script>
+    <link rel="stylesheet" href="Bootstrap/bootstrap.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/vscode.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    <link rel="stylesheet" href="css/course_content.css" />
+
+    <!-- Highlight.js for VSCode style syntax highlighting -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/vs2015.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/javascript.min.js"></script>
+    <script>
+        hljs.highlightAll();
+    </script>
 </head>
 
 <body>
@@ -24,6 +31,19 @@ include "include/session.php";
     include "include/sidebar.php";
     ?>
     <div class="main-content">
+        <div class="background-js">
+            <div>
+                <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="5000">
+                            <img class="d-block w-100" style="height: 800px; object-fit: cover" src="img/coder.PNG" alt="js" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <section class="about-section mb-5">
             <h1 class="section-title">About Us</h1>
         </section>
